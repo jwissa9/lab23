2776d9dafd1c319b1f7967c09a5a9af76a5fb549
Ubuntu <ubuntu@ip-172-31-38-28.ec2.internal>
2020-01-21 17:51:41 +0000

    RubyBasics1 - Part 2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -2,14 +2,26 @@
 # Part I
 def sum arr
   # YOUR CODE HERE
-  total = 0;
-  arr.each {|a| total += a};
-  return total;
+  total = 0
+  arr.each {|a| total += a}
+  return total
 end
 
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  end
+  
+  if arr.size == 1
+    return arr[0]
+  end
+  
+  res = arr.max + arr.sort[-2]
+  
+  return res
+  
 end
 
 # Part III
