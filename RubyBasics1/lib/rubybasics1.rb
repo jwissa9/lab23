9fc16ae68428850b57119f1e4ca07c9be8866bba
Ubuntu <ubuntu@ip-172-31-38-28.ec2.internal>
2020-01-21 16:43:39 +0000

    RubyBasics1 -Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,18 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  total = 0;
+  arr.each {|a| total += a};
+  return total;
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
