f76a050c11677f3870121014506e890568b53642
Ubuntu <ubuntu@ip-172-31-38-28.ec2.internal>
2020-01-28 16:47:51 +0000

    Ruby Basics 2 - Part 3

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -11,9 +11,17 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s[0, 1] == 'a' || s[0,1] == 'e' || s[0, 1] == 'i' || s[0, 1] == 'o' || s[0,1] == 'u' || s[0,1] == 'A' || s[0,1] == 'E' || s[0,1] == 'I' || s[0,1] == 'O' || s[0,1] == 'U' || s[0,1] == '#' || s[0,1] == ''
+    return false
+  end
+  return true
 end
 
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # YOUR CODE HERtrE
+  if s.to_i % 4 == 0 && s.to_s =~ /\b[01]+\b/
+    return true
+  end
+  return false
 end
