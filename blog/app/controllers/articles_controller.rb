298c8a6e239bf0f3d5cd0bbbc642f5b02c00c532
Ubuntu <ubuntu@ip-172-31-38-28.ec2.internal>
2020-03-13 16:09:49 +0000

    Add error messages for title validation

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,7 +1,4 @@
 class ArticlesController < ApplicationController
-    def new
-    end
-    
     def index
         @articles = Article.all
     end
@@ -10,11 +7,18 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def new
+        @article = Article.new
+    end
+    
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 end
 
