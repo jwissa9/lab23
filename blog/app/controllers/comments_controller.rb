9ab05aa0a5bd4db628db70457988865acc9e1fed
Ubuntu <ubuntu@ip-172-31-38-28.ec2.internal>
2020-03-17 22:05:59 +0000

    Added comments to the show view

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     
     private
         def comment_params
-            params.require(:comment).permit(:comment, :body)
+            params.require(:comment).permit(:commenter, :body)
         end
     
 end
